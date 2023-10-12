f67e2983b8896ff213cbc5baf5375734712c27fc
cong0912 <congntc912@gmail.com>
2023-10-12 14:40:05 +0700

    1-upload

---
+++ src/test/java/MathUtilityTest.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import com.ntc.birdcage.core.MathUtility;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class MathUtilityTest {
+    
+// Test case #1 : verify getFactorial(with n = 0)
+    //Steps/Procedures"
+    //      1.Given n=0
+    //      2.Call/invoke getFactorial(n=0)
+    //Expected result:
+    //      The method must return 1 as the result of 0!=1
+    //Status : PASSED | FAILED chờ lycs chạy hàm mới biết trạng thái đúng sai
+    @Test 
+    public void verifyFactorialGivenRighArgument0ReturnsOk(){
+        assertEquals(1, MathUtility.getFactorial(0));
+        
+    }
+     @Test 
+    public void verifyFactorialGivenRighArgument1ReturnsOk(){
+        assertEquals(1, MathUtility.getFactorial(1));
+        
+    }
+      @Test 
+    public void verifyFactorialGivenRighArgument5ReturnsOk(){
+        assertEquals(120, MathUtility.getFactorial(5));
+        
+    }
+}
+
