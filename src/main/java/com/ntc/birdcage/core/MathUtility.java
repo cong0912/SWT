30e473406922f105dbaad4effc693c8efd84a09e
cong0912 <congntc912@gmail.com>
2023-10-16 14:11:43 +0700

    update getF()

--- src/main/java/com/ntc/birdcage/core/MathUtility.java
+++ src/main/java/com/ntc/birdcage/core/MathUtility.java
@@ -24,7 +24,7 @@ public class MathUtility {
             return 1;
         }
 
-        long result = 1;
+        long result = 10;  //giai thừa khởi đầu bằng 1
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
